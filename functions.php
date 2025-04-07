c7fb9235c47dc69e054a2d26408b6ed803514b50
Steve Bader <info@stephenbader.net>
2025-04-07 06:09:47 +0000

    final version

--- functions.php
+++ functions.php
@@ -24,3 +24,37 @@ if ( ! function_exists( 'stevebader_setup' ) ) :
 endif;
 
 add_action( 'after_setup_theme', 'stevebader_setup' );
+
+
+if ( ! function_exists( 'stevebader_styles' ) ) :
+
+    /**
+     * Enqueue styles.
+     *
+     * @since Twenty Twenty-Two 1.0
+     *
+     * @return void
+     */
+    function stevebader_styles() {
+        // Register theme stylesheet.
+        $theme_version = wp_get_theme()->get( 'Version' );
+
+        $version_string = is_string( $theme_version ) ? $theme_version : false;
+        wp_register_style(
+            'stevebader-style',
+            get_template_directory_uri() . '/style.css',
+            array(),
+            $version_string
+        );
+
+        // Enqueue theme stylesheet.
+        wp_enqueue_style( 'stevebader-style' );
+
+    }
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'stevebader_styles' );
+
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
